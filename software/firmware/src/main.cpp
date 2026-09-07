7d2b1d9d03ab46fb26b72d3545042503b846a696
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-09-07 16:20:49 -0700

    NIMBLE advertising (works!)

--- software/firmware/src/main.cpp
+++ software/firmware/src/main.cpp
@@ -1,12 +1,20 @@
-#include <Arduino.h>
+#include <NimBLEDevice.h>
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Starting NimBLE Advertiser...");
+
+  NimBLEDevice::init("NimBLE_Test_Device");
+
+  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+
+  pAdvertising->setScanResponse(true);
+
+  pAdvertising->start();
+  Serial.println("Advertising started! Scan for 'NimBLE_Test_Device' on your phone.");
 }
 
 void loop() {
-
-  Serial.println("Hello World!");
-
-  delay(1000);
+  delay(2000);
+  Serial.println("Hello, nimble!");
 }
