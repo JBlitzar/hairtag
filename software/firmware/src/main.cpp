1ec73ddd48d9299d9acf9492b942f95bc495fdce
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-09-07 16:39:25 -0700

    Revert "cadvertising"
    This reverts commit 180cbe4fb99a47468b9688e29276fcd08486fde8.

--- software/firmware/src/main.cpp
+++ software/firmware/src/main.cpp
@@ -1,39 +1,20 @@
 #include <NimBLEDevice.h>
 
-uint8_t key[28] = { 0x1c, 0x4f, 0xe7, 0xea, 0x90, 0x86, 0xc2, 0x5d, 0xf7, 0x68, 0xb7, 0x9d, 0x57, 0x34, 0x5b, 0x5e, 0x52, 0xeb, 0xe6, 0xb7, 0xc4, 0xaf, 0xa4, 0x59, 0xb7, 0xdc, 0xe7, 0x10 };
-
-int i;
-
 void setup() {
-    i=0;
-    NimBLEDevice::init("");
+  Serial.begin(9600);
+  Serial.println("Starting NimBLE Advertiser...");
 
-    uint8_t addr[6] = {
-        (uint8_t)(key[0] | 0xC0),
-        key[1], key[2], key[3], key[4], key[5]
-    };
+  NimBLEDevice::init("NimBLE_Test_Device");
 
-    NimBLEAdvertisementData adv;
+  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
 
-    std::string mfg;
-    mfg += "\x4c\x00";       // Apple
-    mfg += "\x12\x19";       // Find My / Offline Finding
-    mfg += "\x00";           // state
-    mfg.append((char *)&key[6], 22);
-    mfg += (char)(key[0] >> 6);
-    mfg += "\x00";            // hint
+  pAdvertising->setScanResponse(true);
 
-    adv.setManufacturerData(mfg);
-
-    NimBLEAdvertising *a = NimBLEDevice::getAdvertising();
-    a->setAdvertisementData(adv);
-    a->setAdvertisementType(BLE_GAP_CONN_MODE_NON);
-    a->start();
+  pAdvertising->start();
+  Serial.println("Advertising started! Scan for 'NimBLE_Test_Device' on your phone.");
 }
-
 
 void loop() {
   delay(2000);
-  Serial.printf("Hello, ohs! %d\n", i);
-  i++;
+  Serial.println("Hello, nimble!");
 }
