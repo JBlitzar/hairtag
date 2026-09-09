b2b3dec96444a5c351747fc94f59c68571c8aeed
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-09-08 20:57:12 -0700

    zephyr

--- software/firmware-nrf52840/src/main.cpp
+++ software/firmware-nrf52840/src/main.cpp
@@ -1,13 +1,21 @@
-#include <Arduino.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
 
-void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-}
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS,
+                  BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR),
+    BT_DATA(BT_DATA_NAME_COMPLETE,
+            "XIAO-TEST", 9),
+};
 
-void loop() {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
+int main(void)
+{
+    bt_enable(NULL);
 
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
+    bt_le_adv_start(
+        BT_LE_ADV_CONN,
+        ad, ARRAY_SIZE(ad),
+        NULL, 0);
+
+    return 0;
 }
